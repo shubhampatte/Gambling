47cafe6cd601f615971fefd5204479ea4ed3bb89
shubhampatte <shubhampatte@gmail.com>
2021-10-20 10:57:41 +0530

    [Added]:Gambler game

---
+++ GamblerProblem.java
@@ -0,0 +1,18 @@
+package gamblingGame;
+
+public class GamblerProblem
+{
+	static void gameCaseOne() // created a method 
+	{
+		int gamblerStake = 100; // start with a stake 
+		int betGame = 1;        // bet every game
+		
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("****Welcome to Gambling Simulation Problem****");
+		gameCaseOne(); // method called
+	}
+
+}
